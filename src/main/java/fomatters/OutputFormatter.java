2508e0f9dcfdd9ee841445df89f6cf7694fdedb1
Daniel Reigada <daniel.reigada@gmail.com>
2017-10-31 02:10:23 +0000

    Add number of steps to output

--- src/main/java/fomatters/OutputFormatter.java
+++ src/main/java/fomatters/OutputFormatter.java
@@ -9,12 +9,16 @@ import java.util.stream.Collectors;
 public class OutputFormatter {
 
   public static String format(List<Solver.Move> moves) {
-    return moves.stream()
+    val numberMoves = moves.size();
+
+    val movesStr = moves.stream()
         .map(move -> {
           val robot = IParser.Robot.fromId(move.getRobot()).toString().substring(0, 1).toUpperCase();
           val dir = move.getDir().toString().substring(0, 1).toLowerCase();
           return robot + " " + dir;
         })
         .collect(Collectors.joining("\n"));
+
+    return numberMoves + "\n" + movesStr;
   }
 }
